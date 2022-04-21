d967a6dc1c8ed389ff3809fa11d54e6b7f71de72
epicMonkey13 <56354271+epicMonkey13@users.noreply.github.com>
2022-04-21 22:24:33 +0100

    rendering upon condition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Lake({name}) {
+  return(
+    <div>
+      <h1>Visit {name}</h1>
     </div>
   );
 }
+
+function SkiResort({name}) {
+  return (
+    <div>
+      <h1>Visit {name}</h1>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Jenny Lake" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="JHMR" />
+      ) : (
+        <h1>
+          Come back in the winter or summer!
+        </h1>
+      )
+      }
+    </div>
+  );
+   
+      }
+   
+
+
 
 export default App;
